a371f11d2a58390eb8393ab4b4ed50baa613b0a3
sealtastic <tiranosaurio2604@gmail.com>
2026-02-13 18:53:45 -0600

    Add Brainfuck interpreter in LUA

---
+++ BRAINFUCK.lua
@@ -0,0 +1,7 @@
+q,z={},(0)q.n=function(b)p,x,s,m,r,h,w,a,u=z,z,string.split(b,""),{},"",z,string.byte,math.abs,0x1
+repeat x+=(0x2-u+z)i=w(s[x])if(a((0x2c)-i)-u)*(i-0x2e)*(a(0x3d-i)-u)*(a(0x5c-i)-u)==(z)then
+if(i<0x2e)then if(m[p])and((m[p]>0xff)or(m[p]<z))then end m[p]=(m[p]and(m[p]-(i-0x2c))or(0x2c-i))%(0x100)elseif
+(i<0x2f)then r=r..string.char(m[p])elseif(i<0x3f)then if(p<z)then error''return end p+=(i-0x3d)else if(i<0x5c)then
+t=(h)if(m[p]~=z)then h+=(u)else while(t>=h)do i=w(s[x])x+=(u)if(a(0x5c-i)==u)then t+=(i-0x5c)end
+end h=(t)end else t=(h+u)if(m[p]~=z)then repeat i=w(s[x-u])x-=(u)if(a(0x5c-i)==u)then t+=(i-0x5c)end
+until t==(h)end end end end until(x>=#s)return(r)end return(q)
--- hi.md
+++
@@ -1,1 +0,0 @@
-i never realised idk
