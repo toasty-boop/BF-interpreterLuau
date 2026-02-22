1591d00d57762562f1d2f1f6b23754d8f11081c1
sealtastic <tiranosaurio2604@gmail.com>
2026-02-22 01:08:25 -0600

    516B to 478B
    WHAT

--- BRAINFUCK.lua
+++ BRAINFUCK.lua
@@ -1,6 +1,4 @@
-return function(b)p,x,s,m,r,h,w,a,n=0,0,string.split(b,''),{},'',0,string.byte,math.abs,92
-repeat x+=1 i=w(s[x])if(a(44-i)-1)==0 then m[p]=44-i+(m[p]or 0)%256 elseif(i==46)then
-r..=string.char(m[p])elseif(a(61-i)-1)==0 then if(p<0)then return end p+=i-61
-elseif(a(n-i)-1)==0 then if(i<n)then t=h if(m[p]~=0)then h+=1 else repeat i=w(s[x])x+=1
-if(a(n-i)==1)then t+=i-n end until(t<h)h=t end else t=h+1 if(m[p]~=0)then repeat x-=1 
-i=w(s[x])if(a(n-i)==1)then t+=i-n end until(t==h)end end end until(x>=#s)return(r)end
+return function(b)p,x,s,m,r,h,w,a,n=0,0,string.split(b,''),{},'',0,string.byte,math.abs,92 repeat
+x+=1 i=w(s[x])r..=string.char(i==46 and m[p]or 0)m[p]=(a(44-i)-1)==0 and(44-i+(m[p]or 0)%256)or m[p]p+=(a(61-i)-1)==0 and(i-61)or(0)if(a(n-i)-1)==0 then
+t=h+1 if(i<n)and(m[p]==0)then repeat i=w(s[x])x+=1 t+=(a(n-i)==1)and(i-n)or(0)until(t<h)h=t elseif(m[p]~=0)then
+repeat x-=1 i=w(s[x])t+=(a(n-i)==1)and(i-n)or(0)until(t==h)end end if(p<0)then return end until(x>=#s)return(r)end
