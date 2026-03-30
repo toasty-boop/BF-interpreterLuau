83253e5902d095edf1bcbc4b32cdc77559035ec8
sealtastic <tiranosaurio2604@gmail.com>
2026-03-29 19:23:42 -0600

    422B to 410B
    removed pointer under 0 detection, due to it adding 12 full bytes

--- BRAINFUCK.lua
+++ BRAINFUCK.lua
@@ -1,3 +1,4 @@
 return function(b)p,x,s,m,r,h,w,a=0,0,b,{},'',0,string.byte,math.abs;while(x<#b)do
-s,i,m[p],x=b,w(s,x+1),m[p]or 0,x+1;r..=string.char(i==46 and m[p]or 0)m[p]=a(44-i)<2 and(44-i+m[p]%256)or m[p]p+=p<0 and''or a(61-i)<2 and(i-61)or(0)e,t,f,k=i,h+1,i<92 and m[p]==0,i>=92 and(m[p]~=0)while(a(92-e)<2)and(f)or(k)and
-not((t<h)and(f)or(t==h and k))do x+=f and(1)or-1;i=w(s,x)t+=(a(92-i)<2 and i-92)or(0)end;h=t;end;return(r)end
+s,i,m[p],x=b,w(s,x+1),m[p]or 0,x+1;r..=string.char(i==46 and m[p]or 0)m[p]=a(44-i)<2 and(44-i+m[p]%256)or m[p]p+=a(61-i)<2 and(i-61)or(0)e,t,f,k=i,h+1,i<92 and
+m[p]==0,i>=92 and(m[p]~=0)while(a(92-e)<2)and(f)or(k)and not((t<h)and(f)or(t==h and k))do
+x+=f and(1)or-1;i=w(s,x)t+=(a(92-i)<2 and i-92)or(0)end;h=t;end;return(r)end
