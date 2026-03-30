ca3ec51355ae533503b4d49b5cbfe3cbfbe4a4b7
sealtastic <tiranosaurio2604@gmail.com>
2026-03-29 19:19:09 -0600

    427B to 422B

--- BRAINFUCK.lua
+++ BRAINFUCK.lua
@@ -1,4 +1,3 @@
-return function(b)p,x,s,m,r,h,w,a=0,0,b,{},'',0,string.byte,math.abs;repeat
-x+=1;s,i=b,w(s,x)r..=string.char(i==46 and m[p]or 0)m[p]=a(44-i)<2 and(44-i+(m[p]or 0)%256)or m[p]p+=a(61-i)<2 and(i-61)or(0)if(a(92-i)<2)then
-t=h+1;f,k=i<92 and m[p]==0,i>=92 and(m[p]~=0)if(f)or(k)then repeat x+=f and(1)or-1;i=w(s,x)t+=a(92-i)<2 and(i-92)or(0)until(t<h)and f or(t==h and k)end
-h=t;end;if(p<0)then return;end;until(x>=#b)return(r)end
+return function(b)p,x,s,m,r,h,w,a=0,0,b,{},'',0,string.byte,math.abs;while(x<#b)do
+s,i,m[p],x=b,w(s,x+1),m[p]or 0,x+1;r..=string.char(i==46 and m[p]or 0)m[p]=a(44-i)<2 and(44-i+m[p]%256)or m[p]p+=p<0 and''or a(61-i)<2 and(i-61)or(0)e,t,f,k=i,h+1,i<92 and m[p]==0,i>=92 and(m[p]~=0)while(a(92-e)<2)and(f)or(k)and
+not((t<h)and(f)or(t==h and k))do x+=f and(1)or-1;i=w(s,x)t+=(a(92-i)<2 and i-92)or(0)end;h=t;end;return(r)end
